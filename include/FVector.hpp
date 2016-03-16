b1e28422eb8f6e196df7c2f64ce0f44996ef828b
Ibrahim Timothy Onogu <captaintim@live.com>
2016-03-16 23:02:09 +0100

    Added license....

--- include/FVector.hpp
+++ include/FVector.hpp
@@ -1,3 +1,15 @@
+/* 
+ * LICENSE
+ * =======
+ * Distributed under the Boost Software License, Version 1.0.
+ *      (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * Author: Ibrahim Timothy Onogu
+ * Email:  ionogu@acm.org
+ * Date:   DD/MM/2016
+ */
+
 #ifndef FVECTOR_H
 #define FVECTOR_H
 
