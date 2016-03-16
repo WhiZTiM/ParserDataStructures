0f9f5ecf9bd7bb8606de0646172937fca23c7295
Ibrahim Timothy Onogu <captaintim@live.com>
2016-03-16 23:29:29 +0100

    Added license....

--- include/String.hpp
+++ include/String.hpp
@@ -1,3 +1,15 @@
+/* 
+ * LICENSE
+ * =======
+ * Distributed under the Boost Software License, Version 1.0.
+ *      (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * Author: Ibrahim Timothy Onogu
+ * Email:  ionogu@acm.org
+ * Date:   DD/MM/2016
+ */
+ 
 #ifndef STRING_H
 #define STRING_H
 #include "Config.hpp"
